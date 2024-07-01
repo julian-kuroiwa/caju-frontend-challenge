30d851f15b674c191740db4c35e6b2cbc0c918fb
Julian Kuroiwa <jkuroiwa@DESKTOP-DRC662E>
2024-07-01 16:00:52 -0300

    feat: get registrations on dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,30 @@
 import Collumns from "./components/Columns";
 import * as S from "./styles";
 import { SearchBar } from "./components/Searchbar";
+import request from "~/api";
+import { useEffect, useState } from "react";
 
 const DashboardPage = () => {
+  const [registrations, setRegistrations] = useState<any>()
+
+  const getRegistrations = async () => {
+    try {
+      const response = await request.get('/registrations')
+
+      return response.data
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  useEffect(() => {
+    getRegistrations().then(data => setRegistrations(data))
+  }, [])
+
   return (
     <S.Container>
       <SearchBar />
-      <Collumns registrations={[]} />
+      <Collumns registrations={registrations} />
     </S.Container>
   );
 };
