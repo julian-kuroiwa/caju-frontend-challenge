3fd3b94b9e196eaa7b6c8ab961c3d7c1d86c9424
Julian Kuroiwa <julian.kuroiwa@gmail.com>
2024-07-01 23:10:35 -0300

    feat: Add handle actions on registrationCard

--- src/pages/Dashboard/components/RegistrationCard/index.tsx
+++ src/pages/Dashboard/components/RegistrationCard/index.tsx
@@ -6,32 +6,37 @@ import {
   HiOutlineCalendar,
   HiOutlineTrash,
 } from "react-icons/hi";
+import { useContext } from "react";
+import { Registration, RegistrationContext } from "~/contexts/RegistrationsContext";
 
 type Props = {
-  data: any;
+  data: Registration;
 };
 
-const RegistrationCard = (props: Props) => {
+const RegistrationCard = ({data}: Props) => {
+  const {handleStatus, handleRemove} = useContext(RegistrationContext)
+  const {employeeName, email, admissionDate, id} = data
+
   return (
     <S.Card>
       <S.IconAndText>
         <HiOutlineUser />
-        <h3>{props.data.employeeName}</h3>
+        <h3>{employeeName}</h3>
       </S.IconAndText>
       <S.IconAndText>
         <HiOutlineMail />
-        <p>{props.data.email}</p>
+        <p>{email}</p>
       </S.IconAndText>
       <S.IconAndText>
         <HiOutlineCalendar />
-        <span>{props.data.admissionDate}</span>
+        <span>{admissionDate}</span>
       </S.IconAndText>
       <S.Actions>
-        <ButtonSmall bgcolor="rgb(255, 145, 154)" >Reprovar</ButtonSmall>
-        <ButtonSmall bgcolor="rgb(155, 229, 155)">Aprovar</ButtonSmall>
-        <ButtonSmall bgcolor="#ff8858">Revisar novamente</ButtonSmall>
+        <ButtonSmall bgcolor="rgb(255, 145, 154)" onClick={() => handleStatus({registration: data, status: 'REPROVED'})}>Reprovar</ButtonSmall>
+        <ButtonSmall bgcolor="rgb(155, 229, 155)" onClick={() => handleStatus({registration: data, status: 'APPROVED'})}>Aprovar</ButtonSmall>
+        <ButtonSmall bgcolor="#ff8858" onClick={() => handleStatus({registration: data, status: 'REVIEW'})}>Revisar novamente</ButtonSmall>
 
-        <HiOutlineTrash />
+        <HiOutlineTrash role="button" onClick={() => handleRemove(id)} />
       </S.Actions>
     </S.Card>
   );
